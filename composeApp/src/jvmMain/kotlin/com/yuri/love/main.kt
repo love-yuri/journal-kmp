c9f30dcf8c3595f69e25724ea08fcbe8e8f810d2
love-yuri <yuri2078170658@gmail.com>
2025-08-18 20:16:04 +0800

    feat: add init size

--- composeApp/src/jvmMain/kotlin/com/yuri/love/main.kt
+++ composeApp/src/jvmMain/kotlin/com/yuri/love/main.kt
@@ -1,8 +1,11 @@
 package com.yuri.love
 
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowSize
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
@@ -11,6 +14,7 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "Journal",
         state = WindowState().apply {
+            size = DpSize(500.dp, 1000.dp)
             position = WindowPosition(Alignment.Center)
         }
     ) {
