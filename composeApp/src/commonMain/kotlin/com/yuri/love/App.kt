c01ea389713d47e8f46ea1c07a3e7c91517fbfba
love-yuri <yuri2078170658@gmail.com>
2025-08-19 20:07:23 +0800

    style: 移除多余引用

--- composeApp/src/commonMain/kotlin/com/yuri/love/App.kt
+++ composeApp/src/commonMain/kotlin/com/yuri/love/App.kt
@@ -1,27 +1,10 @@
 package com.yuri.love
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
-import com.yuri.love.views.HomeScreen
-import io.github.oshai.kotlinlogging.KotlinLogging.logger
-import org.jetbrains.compose.resources.painterResource
+import com.yuri.love.views.home.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import journal.composeapp.generated.resources.Res
-import journal.composeapp.generated.resources.compose_multiplatform
-import journal.composeapp.generated.resources.more
 
 expect object Static {
     fun init()
