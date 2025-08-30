f21364c76f29b239db59874ed320cdde0f758a8e
love-yuri <yuri2078170658@gmail.com>
2025-08-30 13:52:32 +0800

    fix: 修改使用的抽屉

--- composeApp/src/commonMain/kotlin/com/yuri/love/views/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yuri/love/views/home/HomeScreen.kt
@@ -1,10 +1,7 @@
 package com.yuri.love.views.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
@@ -17,8 +14,8 @@ import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.yuri.love.styles.GlobalColors
 import com.yuri.love.views.home.components.DiaryHeaderAdvanced
-import com.yuri.love.views.home.components.HomeDrawer
 import com.yuri.love.views.home.components.JournalCardComposable
+import com.yuri.love.views.home.components.LovelyEnhancedDrawer
 import com.yuri.love.views.home.components.TapBar
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -52,7 +49,7 @@ private fun CreateHome() {
         drawerContent = {
             ModalDrawerSheet {
                 // 抽屉内容
-                HomeDrawer(
+                LovelyEnhancedDrawer(
                     onCloseDrawer = {
                         scope.launch {
                             drawerState.close()
