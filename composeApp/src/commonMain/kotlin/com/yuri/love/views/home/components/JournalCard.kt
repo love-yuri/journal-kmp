35763d372503aa58cd5411ea705970be49233871
love-yuri <yuri2078170658@gmail.com>
2025-09-01 19:33:18 +0800

    style: 优化样式

--- composeApp/src/commonMain/kotlin/com/yuri/love/views/home/components/JournalCard.kt
+++ composeApp/src/commonMain/kotlin/com/yuri/love/views/home/components/JournalCard.kt
@@ -3,21 +3,26 @@ package com.yuri.love.views.home.components
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.text.TextStyle
@@ -29,21 +34,21 @@ import journal.composeapp.generated.resources.Res
 import journal.composeapp.generated.resources.date
 import journal.composeapp.generated.resources.weather_sun
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 /**
  * 日记卡片绘制
  */
 @Composable
+@Preview
 fun JournalCardComposable() {
-    Card(
+    Surface(
         modifier = Modifier
             .fillMaxWidth()
-        ,
-        shape = RoundedCornerShape(12.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White
-        ),
-        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp)
+            .border(width = 1.dp, color = Color(0xFFFFB3D1),
+                shape = RoundedCornerShape(10.dp))
+            ,
+        shape = RoundedCornerShape(10.dp),
     ) {
         Column(modifier = Modifier.padding(all = 12.dp)) {
             Box(modifier = Modifier.fillMaxWidth()) {
@@ -61,6 +66,7 @@ fun JournalCardComposable() {
                     )
                 }
             }
+            Spacer(Modifier.height(2.dp))
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Image(
                     painterResource(Res.drawable.date),
@@ -75,6 +81,7 @@ fun JournalCardComposable() {
                 )
                 Text("晴天", modifier = Modifier.padding(start = 8.dp), fontFamily = GlobalFonts.MapleMonoFont)
             }
+            Spacer(Modifier.height(2.dp))
             Row(
                 modifier = Modifier.padding(top = 5.dp),
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -84,26 +91,26 @@ fun JournalCardComposable() {
                 Text(
                     text = "#生活",
                     style = TextStyle(
-                        color = Color(0xFF9E3B46),  // text-pink-800
-                        fontSize = 13.sp,  // text-xs
+                        color = Color(0xFF7C3AED),
+                        fontSize = 13.sp,
                         fontWeight = FontWeight.Medium
                     ),
                     modifier = Modifier
-                        .background(Color(0xFFFEC7D7), shape = CircleShape)  // bg-pink-100 + rounded-full
-                        .padding(horizontal = 12.dp, vertical = 3.dp)  // 内外边距
+                        .background(Color(0xFFFEC7D7), shape = CircleShape)
+                        .padding(horizontal = 12.dp, vertical = 3.dp)
                 )
 
                 // 标签 2
                 Text(
                     text = "#心情",
                     style = TextStyle(
-                        color = Color(0xFF9B2D20),  // text-pink-800
-                        fontSize = 13.sp,  // text-xs
+                        color = Color(0xFFDB2777),
+                        fontSize = 13.sp,
                         fontWeight = FontWeight.Medium
                     ),
                     modifier = Modifier
-                        .background(Color(0xFFFEC7D7), shape = CircleShape)  // bg-pink-100 + rounded-full
-                        .padding(horizontal = 12.dp, vertical = 3.dp)  // 内外边距
+                        .background(Color(0xFFFEC7D7), shape = CircleShape)
+                        .padding(horizontal = 12.dp, vertical = 3.dp)
                 )
             }
         }
