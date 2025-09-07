a83ad1241a2be640c1fc08c9c9e0c5d42870b770
love-yuri <yuri2078170658@gmail.com>
2025-09-07 20:33:09 +0800

    feat: 添加全局共享常量

---
+++ composeApp/src/commonMain/kotlin/com/yuri/love/share/Constant.kt
@@ -0,0 +1,21 @@
+package com.yuri.love.share
+
+/**
+ * 数据库文件后缀
+ */
+const val DatabaseSuffix: String = "db"
+
+/**
+ * 日记数据库名称
+ */
+const val JournalDatabaseName: String = "journal"
+
+/**
+ * 天气的api
+ */
+const val WeatherApiKey: String = "S4gOH8lQuBXpjpLGv"
+
+/**
+ * 天气api地址
+ */
+const val WeatherApiUrl: String = "https://api.seniverse.com/v3/"
