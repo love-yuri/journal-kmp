6331bf5fff65e3459d77eddbe9798c020d83a74e
love-yuri <yuri2078170658@gmail.com>
2025-09-11 21:47:04 +0800

    fix: 删除InputText组件及引用

--- composeApp/src/commonMain/kotlin/com/yuri/love/views/home/TestScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yuri/love/views/home/TestScreen.kt
@@ -2,11 +2,9 @@ package com.yuri.love.views.home
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
-import com.yuri.love.views.create.components.InputText
 
 class TestScreen: Screen {
     @Composable
     override fun Content() {
-        InputText()
     }
 }
