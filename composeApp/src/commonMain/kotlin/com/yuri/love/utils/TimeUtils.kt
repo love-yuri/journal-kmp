a16918d9bc792f51bef253920d0f22438881c8cb
love-yuri <yuri2078170658@gmail.com>
2025-09-13 14:21:38 +0800

    feat: 优化日记卡片UI与时间工具

--- composeApp/src/commonMain/kotlin/com/yuri/love/utils/TimeUtils.kt
+++ composeApp/src/commonMain/kotlin/com/yuri/love/utils/TimeUtils.kt
@@ -1,9 +1,29 @@
 package com.yuri.love.utils
 
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.number
+import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 object TimeUtils {
     @OptIn(ExperimentalTime::class)
     val now: Long get() = Clock.System.now().toEpochMilliseconds()
+
+    @OptIn(ExperimentalTime::class)
+    fun formatTimestampDay(timestampMillis: Long): String {
+        val instant = Instant.fromEpochMilliseconds(timestampMillis)
+        val localDate = instant.toLocalDateTime(TimeZone.currentSystemDefault()).date
+
+        return "${localDate.year}年${localDate.month.number}月${localDate.day}日"
+    }
+
+    @OptIn(ExperimentalTime::class)
+    fun formatTimestampTime(timestampMillis: Long): String {
+        val instant = Instant.fromEpochMilliseconds(timestampMillis)
+        val localDate = instant.toLocalDateTime(TimeZone.currentSystemDefault()).time
+
+        return "${localDate.hour}时${localDate.minute}分"
+    }
 }
