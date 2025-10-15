6bc708e8276a14c3eee7d76ccc8dfc82ebbbd723
love-yuri <yuri2078170658@gmail.com>
2025-10-15 19:06:26 +0800

    feat: 添加全局导航器与页面跳转功能

--- composeApp/src/commonMain/kotlin/com/yuri/love/share/GlobalValue.kt
+++ composeApp/src/commonMain/kotlin/com/yuri/love/share/GlobalValue.kt
@@ -3,7 +3,18 @@ package com.yuri.love.share
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.Navigator
 
 object GlobalValue {
     var weather by mutableStateOf("loading...")
+    private var _navigator: Navigator? = null
+
+    fun bindNavigator(navigator: Navigator) {
+        _navigator = navigator
+    }
+
+    fun push(screen: Screen) {
+        _navigator?.push(screen)
+    }
 }
