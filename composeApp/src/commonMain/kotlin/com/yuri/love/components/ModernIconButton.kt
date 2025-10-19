1308a06e43a26777163234bacf030f76df6ab068
love-yuri <yuri2078170658@gmail.com>
2025-10-19 12:00:45 +0800

    feat: 新增button和topBar组件

---
+++ composeApp/src/commonMain/kotlin/com/yuri/love/components/ModernIconButton.kt
@@ -0,0 +1,34 @@
+package com.yuri.love.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun ModernIconButton(
+    icon: ImageVector,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tint: Color = Color.White,
+    enabled: Boolean = true
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier.size(40.dp),
+        enabled = enabled
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = contentDescription,
+            tint = if (enabled) tint else tint.copy(alpha = 0.5f),
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
