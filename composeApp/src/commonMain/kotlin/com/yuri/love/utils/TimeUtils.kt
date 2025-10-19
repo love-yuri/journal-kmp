e75b254de7ace13d3e5649c4edc93f7c4484523b
love-yuri <yuri2078170658@gmail.com>
2025-10-19 16:14:37 +0800

    feat: 添加时间计算与统计信息展示功能

--- composeApp/src/commonMain/kotlin/com/yuri/love/utils/TimeUtils.kt
+++ composeApp/src/commonMain/kotlin/com/yuri/love/utils/TimeUtils.kt
@@ -1,9 +1,17 @@
 package com.yuri.love.utils
 
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
+import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format.DateTimeFormat
 import kotlinx.datetime.number
+import kotlinx.datetime.periodUntil
+import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
+import java.time.format.DateTimeFormatter
 import kotlin.time.Clock
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
@@ -28,5 +36,19 @@ object TimeUtils {
         val localDate = instant.toLocalDateTime(TimeZone.currentSystemDefault()).time
 
         return "${localDate.hour}时${localDate.minute}分"
+    }
+
+    @OptIn(ExperimentalTime::class)
+    fun calculateTimeDifference(targetTime: String): String {
+        val zone = TimeZone.currentSystemDefault()
+
+        val formatter = LocalDateTime.parse(targetTime.replace(" ", "T"))
+        val now = nowTime.toLocalDateTime(zone)
+
+        val startInstant = formatter.toInstant(zone)
+        val nowInstant = now.toInstant(zone)
+        val duration: Duration = nowInstant - startInstant
+
+        return "${duration.inWholeDays}天"
     }
 }
