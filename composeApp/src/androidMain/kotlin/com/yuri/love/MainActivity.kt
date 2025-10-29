bb60599c34b3a3628bfec6648b22c659e3b5980c
love-yuri <yuri2078170658@gmail.com>
2025-10-29 18:53:07 +0800

    feat: 添加指纹认证与权限配置

--- composeApp/src/androidMain/kotlin/com/yuri/love/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/yuri/love/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.yuri.love
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.biometric.BiometricPrompt
+import androidx.fragment.app.FragmentActivity
 import com.yuri.love.database.DriverFactory
+import com.yuri.love.database.SystemConfig
+import com.yuri.love.utils.BiometricAuth
 
 @OptIn(ExperimentalMultiplatform::class)
 actual object Static {
@@ -15,20 +17,35 @@ actual object Static {
     }
 }
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         DriverFactory.context = this
         enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
 
-        setContent {
-            App()
-        }
+        val biometricAuth = BiometricAuth(this)
+        biometricAuth.authenticate(
+            title = "登录Journal",
+            subtitle = "请使用指纹认证",
+            negativeButtonText = "取消",
+            onSuccess = {
+                setContent {
+                    App()
+                }
+            },
+            onError = { errorCode, errString ->
+                // 如果不是用户主动取消，就退出程序
+                if (errorCode != BiometricPrompt.ERROR_USER_CANCELED &&
+                    errorCode != BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                    Toast.makeText(this@MainActivity, "认证失败，退出程序", Toast.LENGTH_SHORT).show()
+                }
+                finish()
+            },
+            onFailed = {
+                Toast.makeText(this@MainActivity, "指纹不匹配，退出程序!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        )
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
